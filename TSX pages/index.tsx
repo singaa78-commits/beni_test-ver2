af411210b0610a464f6e7abb2d9f96c25d9da1f3
singaa78-commits <singaa78@gmail.com>
2026-03-13 17:14:51 +0900

    Create index.tsx

---
+++ TSX pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>배포 성공</h1>
+      <p>Vercel에서 정상적으로 열리는 테스트 페이지입니다.</p>
+    </div>
+  );
+}
